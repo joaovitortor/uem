4386f38555304889d671e1567af5e0478c9b6992
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2025-05-26 15:06:48 -0300

    Update ExemploClasseAbstrata.java

--- POO/Codigos/src/Aulas/ExempoClasseAbstrata/classe/abstrata/ExemploClasseAbstrata.java
+++ POO/Codigos/src/Aulas/ExempoClasseAbstrata/classe/abstrata/ExemploClasseAbstrata.java
@@ -5,9 +5,9 @@ public class ExemploClasseAbstrata {
     public static void main(String[] args) {
         Tv tv = new Tv(42, 220);
         
-        tv.setLigado(True);
+        //tv.setLigado(True);
         tv.setCanal(10);
-        tv.desligar();
+        //tv.desligar();
     }
     
 }
